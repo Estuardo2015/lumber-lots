b1a0c92e55728a9f8db9061d9de553b085cd25b0
Estuardo Valenzuela <eiv10@humboldt.edu>
2018-03-23 19:48:57 -0700

    header

--- lumberjack-lots/App.js
+++ lumberjack-lots/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements'
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>LumberJack Lots</Text>
+
+      <View style={{ flex: 1, backgroundColor: '#ddd'}}>
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'Lumberjack Lots', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'info', color: '#fff' }}
+          outerContainerStyles={{ backgroundColor: '#1B660F' }}
+        />
       </View>
+
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
